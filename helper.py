d30a14ecc11037ce7ec9ac4f6cf63de6f50cd36d
Akhilesh Srivastava <as5215@columbia.edu>
2018-01-06 21:00:19 +0530

    Combined net_config and setup to helper

--- helper.py
+++ helper.py
@@ -2,6 +2,32 @@ import sys, socket, select, ipaddress, psutil
 from subprocess import *
 import os
 import netifaces as nif
+import redis_lock
+from redlock import RedLock
+
+### Defining the resource files ###
+nmap_xml1 = 'static/nmap_raw1.xml'
+nmap_xml2 = 'static/nmap_raw2.xml'
+hosts_list_file = 'static/hosts_list_file.list'
+hosts_json_file = 'static/json_dictionary.json'
+
+#### Define locks ####
+lock_xml = RedLock("xml_lock")
+lock_host_json_files = RedLock("host_json_files_lock")
+
+### To hold the result of deep-scan ###
+scan_host_result = None
+
+### Remove previously generated resource files ###
+def remove_resource_files():
+    if os.path.isfile(nmap_xml1):
+        os.remove(nmap_xml1)
+    if os.path.isfile(nmap_xml2):
+        os.remove(nmap_xml2)
+    if os.path.isfile(hosts_list_file):
+        os.remove(hosts_list_file)
+    if os.path.isfile(hosts_json_file):
+        os.remove(hosts_json_file)
 
 def get_IP():
     'Finds and returns the local IP address as dotted-quad ints on my host computer.'
@@ -26,7 +52,6 @@ def get_host_mac():
         if if_ip == ip:
             return if_mac
     return None
-
 
 ip = get_IP()
 mac = str(get_host_mac())
